27bdbe1692073785b6142150ed9a52cb23c2fbb6
Soumil Shah <s.shah@jobtarget.com>
2020-08-05 15:47:20 -0400

    Docker File

--- backend/flask/test.py
+++ backend/flask/test.py
@@ -11,20 +11,20 @@ class FlaskTestCase(unittest.TestCase):
     # Check if Response is 200
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get("/fo")
+        response = tester.get("/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
 
     # check if content return is application/json
     def test_index_content(self):
         tester = app.test_client(self)
-        response = tester.get("/fo")
+        response = tester.get("/")
         self.assertEqual(response.content_type, "application/json")
 
     # check for Data returned
     def test_index_data(self):
         tester = app.test_client(self)
-        response = tester.get("/fo")
+        response = tester.get("/")
         self.assertTrue(b'Message' in response.data)
 
 
